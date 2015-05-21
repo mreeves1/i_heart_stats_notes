bde5fc24766a00b12a14ae4892f16319813f4c7e
Michael Reeves <mike.reeves@gmail.com>
2015-05-21 01:14:04 -0700

    Add some front end goodness

--- chi.php
+++ chi.php
@@ -1,5 +1,27 @@
 <?php
+// Calculate Chi Square for I Heart Stats Homework
 require __DIR__ . '/vendor/autoload.php';
 
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Chi Square Calculator</title>
+    <link href="vendor/twitter/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="jumbotron img-rounded" >
+        <h1>Chi Square Calculator</h1>
+      </div>
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+          TODO...
+        </div>
 
-
+      </div>
+    </div>
+  <script src="vendor/components/jquery/jquery.min.js"></script>
+  <script src="vendor/twitter/bootstrap/dist/bootstrap.js"></script>
+  </body>
+</html>
