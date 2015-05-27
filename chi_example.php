c90c79c5a68f5401eec3f7e1ebdee64ac8c66916
Michael Reeves <mreeves@kabam.com>
2015-05-26 17:17:04 -0700

    More formatting changes

--- chi_example.php
+++ chi_example.php
@@ -16,29 +16,28 @@ require __DIR__ . '/classes/Chi.php';
         <div class="col-md-8 col-md-offset-2">
           <?php
           $c1 = new Chi();
-          echo "<h2>Halloween Rent vs. Own</h2>";
+          echo "<h2>Halloween Rent vs. Own</h2>\n";
           $c1->inputFile('halloween.txt');
           $observedValues1 = $c1->calculateObservedValues();
           echo $c1->renderTable($observedValues1);
-          echo "<br/>";
+          echo "<br/>\n";
 
           $c2 = new Chi();
-          echo "<h2>Birthing</h2>";
+          echo "<h2>Birthing</h2>\n";
           $c2->inputFile('birthing.txt');
           $observedValues2 = $c2->calculateObservedValues();
           echo $c2->renderTable($observedValues2);
-          echo "<br/>";
+          echo "<br/>\n";
 
           $c3 = new Chi();
-          echo "<h2>Company Party</h2>";
+          echo "<h2>Company Party</h2>\n";
           $c3->inputFile('company_party.txt');
           $observedValues3 = $c3->calculateObservedValues();
           echo $c3->renderTable($observedValues3);
-          echo "<br/>";
+          echo "<br/>\n";
 
-          echo "chi square value for a significance level of .050 and df of 1 is ".Chi::getChiSquareCriticalValue('.050', 1).".";
-          echo "<br/>";
-          echo "<br/>";
+          echo "chi square value for a significance level of .050 and df of 1 is ".Chi::getChiSquareCriticalValue('.050', 1).".\n";
+          echo "<br/><br/>\n";
           ?>
         </div>
 
